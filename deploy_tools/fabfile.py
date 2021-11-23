fe77a179b5b1b32295ec2f7bd75f9ca25acd8ba3
asuresh231 <asuresh231@gmail.com>
2021-11-23 09:03:15 -0500

    commit 11/23

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,7 +5,7 @@ from fabric.api import cd, env, local, run
 REPO_URL = 'https://github.com/iluvufubu/learndjango.git'
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.hostdef }'
+    site_folder = f'/home/{env.user}/sites/{env.host}'
     run(f'mkdir -p {site_folder}')
     with cd(site_folder):
         _get_latest_source()
